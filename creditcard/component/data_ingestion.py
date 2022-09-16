abd1c90a62994a25a85910bbe0224b33bd032f15
vishalsingh17 <vishal170997@gmail.com>
2022-09-16 14:30:50 +0530

    files updated

---
+++ creditcard/component/data_ingestion.py
@@ -0,0 +1,5 @@
+from creditcard.entity.config_entity import DataIngestionConfig
+from creditcard.entity.config_entity import Da
+import sys, os
+from creditcard.exception import CardException
+from creditcard.logger import logging
